b35d36fb6dea42356f9a2d80a12f0eee9b6c2477
todomato <dreamrrrr@hotmail.com>
2017-04-18 17:24:54 +0800

    init

---
+++ GreedisGood/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using ClassLibrary1;
+
+namespace GreedisGood
+{
+    [TestFixture()]
+    public class UnitTest1
+    {
+        [Test()]
+        public void ShouldBeWorthless()
+        {
+
+            Assert.AreEqual(0, Kata.Score(new int[] { 2, 3, 4, 6, 2 }));
+        }
+    }
+}
